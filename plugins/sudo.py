b7bd01791ed9dffa2818df2408586a120b57b5a4
Ayush81hacker <71370934+Ayush81hacker@users.noreply.github.com>
2021-03-11 09:19:49 +0530

    Add files via upload

--- plugins/sudo.py
+++ plugins/sudo.py
@@ -1,214 +1,85 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
 
-"""
-✘ Commands Available -
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 
-• `{i}addsudo`
-    Add Sudo Users by replying to user or using <space> separated userid(s)
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 
-• `{i}delsudo`
-    Remove Sudo Users by replying to user or using <space> separated userid(s)
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-• `{i}listsudo`
-    List all sudo users.
-"""
+import os
 
-import re
-
+import heroku3
 from telethon.tl.functions.users import GetFullUserRequest
 
-from . import *
+Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+sudousers = os.environ.get("SUDO_USERS", None)
 
 
-@ultroid_cmd(
-    pattern="addsudo ?(.*)",
-)
-async def _(ult):
-    if Var.BOT_MODE == True:
-        try:
-            if ult.sender_id != Var.OWNER_ID:
-                return await eor(
-                    ult, "You are sudo user, You cant add other sudo user."
-                )
-        except BaseException:
-            pass
+@telebot.on(admin_cmd(pattern="sudo"))
+async def sudo(event):
+    sudo = "True" if Config.SUDO_USERS else "False"
+    users = os.environ.get("SUDO_USERS", None)
+    if sudo == "True":
+        await eor(event, f"**TeleBot**\nSudo - `Enabled`\nSudo user(s) - `{users}`")
     else:
-        if ult.sender_id != ultroid_bot.uid:
-            return await eor(ult, "You are sudo user, You cant add other sudo user.")
-    ok = await eor(ult, "`Updating SUDO Users List ...`")
-    if ult.reply_to_msg_id:
-        replied_to = await ult.get_reply_message()
-        id = replied_to.sender.id
-        user = await ult.client(GetFullUserRequest(int(id)))
-        sed.append(id)
-        if id == ultroid_bot.me.id:
-            return await ok.edit("You cant add yourself as Sudo User...")
-        elif is_sudo(id):
-            return await ok.edit(
-                f"[{user.user.first_name}](tg://user?id={id}) `is already a SUDO User ...`"
-            )
-        elif add_sudo(id):
-            return await ok.edit(
-                f"**Added [{user.user.first_name}](tg://user?id={id}) as SUDO User**"
-            )
-        else:
-            return await ok.edit("`SEEMS LIKE THIS FUNCTION CHOOSE TO BROKE ITSELF`")
+        await eor(event, f"**TeleBot**\nSudo - `Disabled`")
 
-    args = ult.pattern_match.group(1).strip()
 
-    if re.search(r"[\s]", args) is not None:
-        args = args.split(" ")
-        msg = ""
-        sudos = get_sudos()
-        for item in args:
-            user = ""
-            try:
-                user = await ult.client(GetFullUserRequest(int(item)))
-            except BaseException:
-                pass
-            if not hasattr(user, "user"):
-                msg += f"• `{item}` __Invalid UserID__\n"
-            elif item in sudos:
-                msg += f"• [{user.user.first_name}](tg://user?id={item}) __Already a SUDO__\n"
-            elif add_sudo(item.strip()):
-                msg += (
-                    f"• [{user.user.first_name}](tg://user?id={item}) __Added SUDO__\n"
-                )
-            else:
-                msg += f"• `{item}` __Failed to Add SUDO__\n"
-        return await ok.edit(f"**Adding Sudo Users :**\n{msg}")
+@telebot.on(admin_cmd(pattern="prefix"))
+async def handler(event):
+    hndlr = Config.CMD_HNDLR
+    if hndlr == r"\.":
+        x = "."
+    else:
+        x = Config.CMD_HNDLR
 
-    id = args.strip()
-    user = ""
+    sudohndlr = Config.SUDO_HNDLR
+    await eor(event, f"Command Handler - {x}\nSudo Handler - {sudohndlr}")
 
+
+@telebot.on(admin_cmd(pattern="addsudo(?: |$)"))
+async def tb(event):
+    ok = await eor(event, "Adding user as a sudo...")
+    telebot = "SUDO_USERS"
+    if Var.HEROKU_APP_NAME is not None:
+        app = Heroku.app(Var.HEROKU_APP_NAME)
+    else:
+        await ok.edit("`[HEROKU]:" "\nPlease setup your` **HEROKU_APP_NAME**")
+        return
+    heroku_var = app.config()
+    if event is None:
+        return
     try:
-        user = await ult.client(GetFullUserRequest(int(i)))
-    except BaseException:
-        pass
-
-    if not id.isdigit():
-        return await ok.edit("`Integer(s) Expected`")
-    elif not hasattr(user, "user"):
-        return await ok.edit("`Invalid UserID`")
-    elif is_sudo(id):
-        return await ok.edit(
-            f"[{user.user.first_name}](tg://user?id={id}) `is already a SUDO User ...`"
-        )
-    elif add_sudo(id):
-        return await ok.edit(
-            f"**Added [{user.user.first_name}](tg://user?id={id}) as SUDO User**"
-        )
+        target = await get_user(event)
+    except Exception:
+        await ok.edit(f"Reply to a user.")
+    if sudousers:
+        newsudo = f"{sudousers} {target}"
     else:
-        return await ok.edit(f"**Failed to add `{id}` as SUDO User ... **")
+        newsudo = f"{target}"
+    await ok.edit(f"Added `{target}` as a sudo user. Restarting.. Give me a minute...")
+    heroku_var[telebot] = newsudo
 
 
-@ultroid_cmd(
-    pattern="delsudo ?(.*)",
-)
-async def _(ult):
-    if Var.BOT_MODE == True:
-        try:
-            if ult.sender_id != Var.OWNER_ID:
-                return await eor(
-                    ult, "You are sudo user, You cant add other sudo user."
-                )
-        except BaseException:
-            pass
-    else:
-        if ult.sender_id != ultroid_bot.uid:
-            return await eor(ult, "You are sudo user, You cant add other sudo user.")
-    ok = await eor(ult, "`Updating SUDO Users List ...`")
-    if ult.reply_to_msg_id:
-        replied_to = await ult.get_reply_message()
-        id = replied_to.sender.id
-        user = await ult.client(GetFullUserRequest(int(id)))
-        sed.remove(id)
-        if not is_sudo(id):
-            return await ok.edit(
-                f"[{user.user.first_name}](tg://user?id={id}) `wasn't a SUDO User ...`"
-            )
-        elif del_sudo(id):
-            return await ok.edit(
-                f"**Removed [{user.user.first_name}](tg://user?id={id}) from SUDO User(s)**"
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.forward.sender_id)
             )
         else:
-            return await ok.edit("`SEEMS LIKE THIS FUNCTION CHOOSE TO BROKE ITSELF`")
-
-    args = ult.pattern_match.group(1)
-
-    if re.search(r"[\s]", args) is not None:
-        args = args.split(" ")
-        msg = ""
-        sudos = get_sudos()
-        for item in args:
-            user = ""
-            try:
-                user = await ult.client(GetFullUserRequest(int(item)))
-            except BaseException:
-                pass
-            if not hasattr(user, "user"):
-                msg += f"• `{item}` __Invalid UserID__\n"
-            elif item in sudos and del_sudo(item):
-                msg += (
-                    f"• [{user.user.first_name}](tg://user?id={id}) __Removed SUDO__\n"
-                )
-            elif item not in sudos:
-                msg += (
-                    f"• [{user.user.first_name}](tg://user?id={id}) __Wasn't a SUDO__\n"
-                )
-            else:
-                msg += f"• `{item}` __Failed to Remove SUDO__\n"
-        return await ok.edit(msg)
-
-    id = args.strip()
-    user = ""
-
-    try:
-        user = await ult.client(GetFullUserRequest(int(i)))
-    except BaseException:
-        pass
-
-    if not id.isdigit():
-        return await ok.edit("`Integer(s) Expected`")
-    elif not hasattr(user, "user"):
-        return await ok.edit("`Invalid UserID`")
-    elif not is_sudo(id):
-        return await ok.edit(
-            f"[{user.user.first_name}](tg://user?id={id}) wasn't a SUDO user ..."
-        )
-    elif del_sudo(id):
-        return await ok.edit(
-            f"**Removed [{user.user.first_name}](tg://user?id={id}) from SUDO User**"
-        )
-    else:
-        return await ok.edit(f"**Failed to Remove `{id}` as SUDO User ... **")
-
-
-@ultroid_cmd(
-    pattern="listsudo$",
-)
-async def _(ult):
-    ok = await eor(ult, "`...`")
-    sudos = get_sudos()
-    if "" in sudos:
-        return await ok.edit("`No SUDO User was assigned ...`")
-    msg = ""
-    for i in sudos:
-        user = ""
-        try:
-            user = await ok.client(GetFullUserRequest(int(i.strip())))
-        except BaseException:
-            pass
-        if hasattr(user, "user"):
-            msg += f"• [{user.user.first_name}](tg://user?id={i}) ( `{i}` )\n"
-        else:
-            msg += f"• `{i}` -> Invalid User\n"
-    return await ok.edit(f"**List of SUDO Users :**\n{msg}")
-
-
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.sender_id)
+            )
+    target = replied_user.user.id
+    return target
--- plugins/updater.py
+++ plugins/updater.py
@@ -1,9 +1,13 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# For TeleBot
+# Copyright (C) 2019 The Raphielscape Company LLC.
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+# credits to @AvinashReddy3108
+# Rewritten for TeleBot by @xditya
+
+"""
+This module is used for updating TeleBot
+"""
 
 import asyncio
 import sys
@@ -12,17 +16,25 @@ from os import environ, execle, path, remove
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-UPSTREAM_REPO_URL = "https://github.com/TeamUltroid/Ultroid"
+from telebot import CMD_HNDLR
+
 requirements_path = path.join(
     path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
 )
 
+HEROKU_API_KEY = Var.HEROKU_API_KEY
+HEROKU_APP_NAME = Var.HEROKU_APP_NAME
+GIT_REPO_NAME = "TeleBot"
+UPSTREAM_REPO_URL = "https://github.com/xditya/TeleBot"
+
+xxxx = CMD_HNDLR if CMD_HNDLR else "."
+
 
 async def gen_chlog(repo, diff):
     ch_log = ""
-    d_form = "On %d/%m/%y at %H:%M:%S"
+    d_form = "On " + "%d/%m/%y" + " at " + "%H:%M:%S"
     for c in repo.iter_commits(diff):
-        ch_log += f"**#{c.count()}** : {c.committed_datetime.strftime(d_form)} : [{c.summary}]({UPSTREAM_REPO_URL.rstrip('/')}/commit/{c}) by `{c.author}`\n"
+        ch_log += f"**#{c.count()}** : {c.committed_datetime.strftime(d_form)} : [{c.summary}]({UPSTREAM_REPO_URL.rstrip('/')}/commit/{c}) by **{c.author}**\n"
     return ch_log
 
 
@@ -40,92 +52,106 @@ async def updateme_requirements():
         return repr(e)
 
 
-@ultroid_cmd(
-    pattern="update ?(.*)",
-)
+@telebot.on(admin_cmd(pattern="update ?(.*)"))
 async def upstream(ups):
-    pagal = await eor(ups, "`Checking for updates, please wait....`")
+    "For .update command, check if the bot is up to date, update if specified"
+    await ups.edit("`Searching for new updates, if any...`")
     conf = ups.pattern_match.group(1)
     off_repo = UPSTREAM_REPO_URL
+    force_updateme = False
+
     try:
-        txt = "`Oops.. Updater cannot continue due to "
+        txt = "`Oops.. Updater cannot continue as "
         txt += "some problems occured`\n\n**LOGTRACE:**\n"
         repo = Repo()
     except NoSuchPathError as error:
-        await eod(pagal, f"{txt}\n`directory {error} is not found`", time=10)
+        await ups.edit(f"{txt}\n`directory {error} is not found`")
         repo.__del__()
         return
     except GitCommandError as error:
-        await eod(pagal, f"{txt}\n`Early failure! {error}`", time=10)
+        await ups.edit(f"{txt}\n`Early failure! {error}`")
         repo.__del__()
         return
     except InvalidGitRepositoryError as error:
         if conf != "now":
-            await eod(
-                pagal,
-                f"**Unfortunately, the directory {error} does not seem to be a git repository.Or Maybe it just needs a sync verification with {GIT_REPO_NAME} But we can fix that by force updating the userbot using** `.update now.`",
-                time=30,
+            await ups.edit(
+                f"**Unfortunately, the directory {error} does not seem to be a git repository.\
+                \nOr Maybe it just needs a sync verification with {GIT_REPO_NAME}\
+            \nBut we can fix that by force updating the userbot using** `{xxxx}update now`."
             )
             return
         repo = Repo.init()
         origin = repo.create_remote("upstream", off_repo)
         origin.fetch()
-        repo.create_head("main", origin.refs.main)
-        repo.heads.main.set_tracking_branch(origin.refs.main)
-        repo.heads.main.checkout(True)
+        force_updateme = True
+        repo.create_head("master", origin.refs.master)
+        repo.heads.master.set_tracking_branch(origin.refs.master)
+        repo.heads.master.checkout(True)
+
     ac_br = repo.active_branch.name
-    if ac_br != "main":
-        await eod(
-            pagal,
-            f"**[UPDATER]:**` You are on ({ac_br})\n Please change to main branch.`",
+    if ac_br != "master":
+        await ups.edit(
+            f"**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). "
+            "in that case, Updater is unable to identify "
+            "which branch is to be merged. "
+            "Please checkout the official branch of TeleBot`"
         )
         repo.__del__()
         return
+
     try:
         repo.create_remote("upstream", off_repo)
     except BaseException:
         pass
+
     ups_rem = repo.remote("upstream")
     ups_rem.fetch(ac_br)
+
     changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
-    if "now" not in conf:
-        if changelog:
-            changelog_str = f"**New UPDATE available for [[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br}):\n\nCHANGELOG**\n\n{changelog}"
-            if len(changelog_str) > 4096:
-                await eor(pagal, "`Changelog is too big, view the file to see it.`")
-                file = open("output.txt", "w+")
-                file.write(changelog_str)
-                file.close()
-                await ups.client.send_file(
-                    ups.chat_id,
-                    "output.txt",
-                    caption=f"Do `{hndlr}update now` to update.",
-                    reply_to=ups.id,
-                )
-                remove("output.txt")
-            else:
-                return await eod(
-                    pagal, f"{changelog_str}\n\nDo `{hndlr}update now` to update."
-                )
-        else:
-            await eod(
-                pagal,
-                f"\n`Your BOT is`  **up-to-date**  `with`  **[[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br})**\n",
-                time=10,
+
+    if not changelog and not force_updateme:
+        await ups.edit(
+            f"\n`Your BOT is`  **up-to-date**  `with`  **[[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br})**\n"
+        )
+        repo.__del__()
+        return
+
+    if conf != "now" and not force_updateme:
+        changelog_str = (
+            f"**New UPDATE available for [[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br}):**\n\n"
+            + "**CHANGELOG**\n\n"
+            + f"{changelog}"
+        )
+        if len(changelog_str) > 4096:
+            await ups.edit("`Changelog is too big, view the file to see it.`")
+            file = open("output.txt", "w+")
+            file.write(changelog_str)
+            file.close()
+            await ups.client.send_file(
+                ups.chat_id,
+                "output.txt",
+                reply_to=ups.id,
             )
-            repo.__del__()
-            return
-    if Var.HEROKU_API is not None:
+            remove("output.txt")
+        else:
+            await ups.edit(changelog_str)
+        await ups.respond(f"Do `{xxxx}update now` to update")
+        return
+
+    if force_updateme:
+        await ups.edit("`Force-Syncing to latest stable userbot code, please wait...`")
+    else:
+        await ups.edit("`Updating userbot, please wait....`")
+    # We're in a Heroku Dyno, handle it's memez.
+    if Var.HEROKU_API_KEY is not None:
         import heroku3
 
-        heroku = heroku3.from_key(Var.HEROKU_API)
+        heroku = heroku3.from_key(Var.HEROKU_API_KEY)
         heroku_app = None
         heroku_applications = heroku.apps()
         if not Var.HEROKU_APP_NAME:
-            await eod(
-                pagal,
-                "`Please set up the HEROKU_APP_NAME variable to be able to update userbot.`",
-                time=10,
+            await ups.edit(
+                "`Please set up the HEROKU_APP_NAME variable to be able to update userbot.`"
             )
             repo.__del__()
             return
@@ -134,20 +160,18 @@ async def upstream(ups):
                 heroku_app = app
                 break
         if heroku_app is None:
-            await eod(
-                pagal,
-                f"{txt}\n`Invalid Heroku credentials for updating userbot dyno.`",
-                time=10,
+            await ups.edit(
+                f"{txt}\n`Invalid Heroku credentials for updating userbot dyno.`"
             )
             repo.__del__()
             return
-        await eor(
-            pagal, "`Userbot dyno build in progress, please wait for it to complete.`"
+        await ups.edit(
+            "`Userbot dyno build in progress, please wait for it to complete.`"
         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
         heroku_git_url = heroku_app.git_url.replace(
-            "https://", "https://api:" + Var.HEROKU_API + "@"
+            "https://", "https://api:" + Var.HEROKU_API_KEY + "@"
         )
         if "heroku" in repo.remotes:
             remote = repo.remote("heroku")
@@ -155,12 +179,12 @@ async def upstream(ups):
         else:
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
-            remote.push(refspec=f"HEAD:refs/heads/{ac_br}", force=True)
+            remote.push(refspec="HEAD:refs/heads/master", force=True)
         except GitCommandError as error:
-            await eod(pagal, f"{txt}\n`Here is the error log:\n{error}`", time=10)
+            await ups.edit(f"{txt}\n`Here is the error log:\n{error}`")
             repo.__del__()
             return
-        await eod(pagal, "`Successfully Updated!\nRestarting, please wait...`", time=60)
+        await ups.edit("`Successfully Updated!\n" "Restarting, please wait...`")
     else:
         # Classic Updater, pretty straightforward.
         try:
@@ -168,11 +192,10 @@ async def upstream(ups):
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         await updateme_requirements()
-        await eod(
-            pagal,
-            "`Successfully Updated!\nBot is restarting... Wait for a second!`",
+        await ups.edit(
+            "`Successfully Updated!\n" "Bot is restarting... Wait for a second!`"
         )
         # Spin a new instance of bot
-        args = [sys.executable, "./resources/startup/deploy.sh"]
+        args = [sys.executable, "-m", "userbot"]
         execle(sys.executable, *args, environ)
         return
